1e640794f87227f97ba1ee67ca03674c36739349
Bharatsaini01 <138390059+Bharatsaini01@users.noreply.github.com>
2025-03-16 19:41:18 +0530

    slove more problems

--- problem.py
+++ problem.py
@@ -194,19 +194,290 @@ def sum_of_even():
     print("sum is :",sum)
 
 # sum_of_even();
+"""
+1111
+123%10 = 3
+0*10
 
+"""
+def reverse_num():
+    num = int(input("enter a number"))
+    reverse_num = 0
+    while num > 0 :
+        digit = num%10
+        num = num//10
+        reverse_num =  reverse_num*10 + digit
+    print(reverse_num)
+
+# reverse_num()
 
 def palindrom_check():
     word = input("enter a word: ")
-    check = 0
-    j = len(word)
-    for i in word:
-        if i == word[j-1]:
-            j -= 1
-            check = 1
-        else:
-            check = 0
-            break
-    print("palindrom") if check == 1 else print("not palindrom")
+    reverse_word = word[::-1]
+    print("palindrom") if word == reverse_word else print("not palindrom")
 
 # palindrom_check()
+
+#  Write a program to print the multiplication table of the number entered by the user, N.
+
+# The table for input N = 5 should get displayed in the following form:
+
+
+def multiplication_table():
+    num = int(input("enter a num"))
+    for i in range(1,11):
+        value = num*i
+        print(value)
+
+# multiplication_table()
+
+# ake an integer N as input, print the corresponding Numeric Inverted Half Pyramid pattern for N.
+
+# For example if N = 4 then pattern will be like:
+
+# 1 2 3 4
+# 1 2 3
+# 1 2
+# 1
+
+def show_descending_number_triangle():
+    N = int(input("Enter a number: "))
+    for i in range(N,0,-1):
+        for j in range(1,i+1):
+            print(j ,end=" ")
+        print("")
+
+# show_descending_number_triangle()
+
+# Problem Description
+
+# Given an integer N as input, print the corresponding Full Numeric Pyramid pattern for N.
+
+# For example if N = 5 then pattern will be like:
+
+# 0 0 0 0 1 0 0 0 0 
+# 0 0 0 2 3 2 0 0 0 
+# 0 0 3 4 5 4 3 0 0
+# 0 4 5 6 7 6 5 4 0
+# 5 6 7 8 9 8 7 6 5 
+
+def full_numeric_pyramid():
+    N = int(input("Enter a Number: "))
+    num = 0
+    for i in range(N,0,-1):
+        for j in range(1,N*2):
+            if j == i :
+                num += 1
+                print(num,end=" ")
+            elif j <= N and j > i:
+                num += 1
+                print(num,end=" ")
+            elif j > N and j <= N*2-i:
+                num -= 1
+                print(num,end=" ")
+            else:
+                print(0,end=" ")
+        print("")
+
+            
+
+        
+        
+
+        
+
+# full_numeric_pyramid()
+
+
+# You are given an integer A, you need to find and return the sum of all the even numbers between 1 and A.
+
+# Even numbers are those numbers that are divisible by 2.
+
+
+def sum_of_even_numbers(A):
+    sum = 0
+    for i in range(1,A):
+        if i%2 ==0:
+            sum += i
+    return sum
+
+# A = 5
+# sum = sum_of_even_numbers(A)
+# print(sum)
+
+
+# Count Even and Odd Digits in a Number
+# Problem: Given a number N, count how many digits are even and how many are odd.
+# Example:
+
+# Input:  482351  
+# Output: Even: 3, Odd: 3  
+
+def count_even_odd_numbers(N):
+    even = 0
+    odd = 0
+    for i in range(1,N+1):
+        if i%2 == 0:
+            even +=1
+        else:
+            odd += 1
+    print("Even : {} Odd : {}".format(even,odd))
+
+# N = 5
+# count_even_odd_numbers(N)
+
+# convert a number to binary without using built-in functions
+# input = 10
+# output = 1010
+
+def convert_number_to_binary(N):
+    binary_number = ""
+    while N > 0:
+        binary_number = str(N % 2) + binary_number
+        N = N//2
+    return binary_number if binary_number else 0
+
+# N = 7
+# binary_value = convert_number_to_binary(N)
+# print(binary_value)
+
+# find the largest and smallest digit in a number
+# input 38576
+# output  largest =8, smallest=3
+
+def find_largest_and_smallest_digit(N):
+    max_digit = N%10
+    min_digit = max_digit
+    while N > 0:
+        N = N//10 
+        next_digit = N%10
+        print(next_digit)
+        if max_digit < next_digit:
+            max_digit = next_digit
+        elif min_digit > next_digit and N != 0:
+            min_digit = next_digit
+    print("Largest: {} and Smallest: {}".format(max_digit,min_digit))
+
+# N = 385762
+# find_largest_and_smallest_digit(N)
+
+# Remove a Specific Digit from a Number
+# Problem: Given a number N and a digit D, remove all occurrences of D from N.
+# Example:
+
+# Input:  N = 10501, D = 0  
+# Output: 151  
+
+def remove_digit_in_number(N,D):
+    result = 0
+    while N > 0:
+        digit = N%10
+        N = N//10
+        if digit == D:
+            continue
+        else:
+            result = digit*10 + result
+        
+    return result
+
+# N,D = 10501,2
+# result = remove_digit_in_number(N,D) 
+# print(result)
+
+
+# convert a number to decimal to binary without using built-in functions 
+# input= 1010
+# output = 10
+
+
+def convert_binary_to_decimal(N):
+    decimal_number = 0
+    i=0
+    while N > 0:
+        digit = N%10
+        N = N//10
+        decimal_number =+ digit * 2**i + decimal_number
+        i += 1
+    print(decimal_number)
+
+# N = 111
+# convert_binary_to_decimal(N)
+
+
+# Problem:
+# Use division by 2 to check if a number is even or odd.
+
+# Example:
+# Input: n = 27
+# Output: Odd
+
+
+def check_even_and_odd(N):
+    if N%2 == 0:
+        print("Even")
+    else:
+        print("Odd")
+
+# N=27
+# check_even_and_odd(N)
+
+
+# Write a function to count the number of 1s in a binary representation of a number using division by 2.
+
+# Example:
+# Input: n = 13 (Binary: 1101)
+# Output: 3
+
+def count_no_of_once_in_binary(N):
+    count = 0
+    while N > 0:
+        digit = N %2
+        N = N//2
+        if digit ==1:
+            count +=1
+    print(count)
+        
+# N=13
+# count_no_of_once_in_binary(N)
+
+
+
+# The parity of a number is even if it has an even number of 1s in its binary representation and odd otherwise.
+
+# Write a function to find the parity of a number using division by 2.
+
+# Example:
+# Input: n = 9 (Binary: 1001, has 2 ones)
+# Output: Even Parity
+
+
+def check_even_odd_parity(N):
+    parity = 0
+    while N > 0 :
+        digit = N%2
+        N = N//2
+        if digit == 1:
+            parity += 1
+
+    print("Even parity") if parity%2 == 0 else print("Odd parity")
+
+# N= 9
+# check_even_odd_parity(N)
+
+# Problem:
+# Write a function to reverse the binary representation of a number using division by 2.
+
+# Example:
+# Input: n = 6 (Binary: 110)
+# Output: 011 (Binary of 3)
+
+def reverse_binary(N):
+    reverse_binary = ""
+    while N>0:
+        reverse_binary = reverse_binary + str(N%2)
+        N = N//2
+    
+    print(reverse_binary) if reverse_binary else 0
+
+N=6
+# reverse_binary(N)

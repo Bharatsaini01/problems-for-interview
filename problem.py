c6336fce56509de9c3a09aa270a3565a170b614a
Bharatsaini01 <138390059+Bharatsaini01@users.noreply.github.com>
2025-04-11 13:02:20 +0530

    slove more problems

--- problem.py
+++ problem.py
@@ -481,3 +481,272 @@ def reverse_binary(N):
 
 N=6
 # reverse_binary(N)
+
+
+# ##############################################   string problems   ################################################
+
+
+# Find All Substrings of a String
+# Problem: Given a string, print all possible substrings.
+# Example:
+# Input: "abc"
+# Output: "a", "ab", "abc", "b", "bc", "c"
+
+
+
+def get_all_sub_string(S):
+    sub_strings = set()
+    for i in range(len(S)):
+        for j in range(i+1,len(S)+1):
+            print(S[i:j])
+            sub_strings.add(S[i:j])
+    return sub_strings
+
+
+# S = "abc"
+# print(", ".join((get_all_sub_string(S))))
+
+# Check if One String is a Substring of Another
+# Problem: Given two strings s1 and s2, check if s2 is a substring of s1.
+# Example:
+# Input: s1 = "hello world", s2 = "world"
+# Output: True
+
+def check_sub_string(s1,s2):
+    if s2 in s1:
+        return True
+    return False
+
+# s1,s2 = "hello world" ,"world"
+# print(check_sub_string(s1,s2))
+
+# find the Longest Palindromic Substring
+# Problem: Given a string, find the longest substring that is a palindrome.
+# Example:
+# Input: "babad"
+# Output: "bab" or "aba"
+
+
+def logest_palindromic_string(S):
+    sub_str = ""
+    for i in range(len(S),0,-1):
+        for j in range(len(S)+1- i):
+            sub_str += S[j:i]
+            if sub_str == sub_str[::-1]:
+                return sub_str
+            sub_str =""
+            j+=1
+            i+=1
+            
+
+            
+# S = "babad"
+# print(logest_palindromic_string(S))
+
+# Count the Number of Vowels and Consonants
+# Problem: Given a string, count the number of vowels and consonants in it.
+# Example:
+# Input: "hello"
+# Output: Vowels: 2, Consonants: 3
+
+
+
+def count_vowels_and_consonants(S):
+    vowels = consonants = 0
+    for i in S:
+        if i in "aeiou":
+            vowels +=1
+        else:
+            consonants +=1
+    print("vowels: {}, consonants: {}".format(vowels,consonants))
+
+# S = "hello"
+# count_vowels_and_consonants(S)
+
+
+
+# Reverse a String
+# Problem: Given a string, return its reverse.
+# Example:
+
+# Input: "hello"
+# Output: "olleh"
+
+
+
+def reverse_string(S):
+    # print(S[::-1])  or
+    reverse_string_1 = "" 
+    for i in S:
+        reverse_string_1 = i + reverse_string_1 
+    
+    print(reverse_string_1)
+
+
+# S = "hello"
+# reverse_string(S)
+
+
+# Check if a String is a Palindrome
+# Problem: Given a string, check if it reads the same forward and backward.
+# Example:
+# Input: "racecar"
+# Output: True
+
+
+def check_palindrom_string(S):
+    if S == S[::-1]:
+        return True
+    return False
+
+# S = "racecar"
+# print(check_palindrom_string(S))
+
+
+# Find the First Non-Repeating Character
+# Problem: Given a string, find the first character that does not repeat.
+# Example:
+# Input: "aabbccdeff"
+# Output: "d"
+
+
+def non_repeating_character(S):
+    for i in S:
+        if S.count(i) < 2:
+            return i
+
+# S = "aabbccdeff"
+# print(non_repeating_character(S))
+
+
+# Remove Duplicates from a String
+# Problem: Given a string, remove duplicate characters while keeping the order.
+# Example:
+
+# Input: "banana"
+# Output: "ban"
+
+
+def remove_duplicate_string(S):
+    result = ""
+    for i in S:
+        if i not in result:
+            result += i
+    return result
+    
+# S ="banana"
+# print(remove_duplicate_string(S))
+
+# Count the Frequency of Each Character
+# Problem: Given a string, count how often each character appears.
+# Example:
+
+# Input: "apple"
+# Output: {'a': 1, 'p': 2, 'l': 1, 'e': 1}
+
+
+def frequency_of_char(S):
+    result = {}
+    for i in S:
+        count =0
+        for j in S:
+            if i == j:
+                count +=1
+        result[i]=count
+    return result
+
+# S = "apple"
+# print(frequency_of_char(S))
+
+# Find the Most Frequent Character
+# Problem: Given a string, find the character that appears the most.
+# Example:
+
+# Input: "mississippi"
+# Output: "i"
+
+
+
+def frequent_character(S):
+    result = {}
+    count = 0
+    for i in S:
+        for j in S:
+            if i == j:
+                count +=1
+        result[i]=count
+        count = 0
+    max_value = max(result.values())
+    print(result.items())
+    f_char = [key for key, value in result.items() if value == max_value]
+    return f_char[0]
+
+# S = "mississippi"
+# print(frequent_character(S))
+
+# Convert a String to an Integer
+# Problem: Convert a numeric string into an integer without using int().
+# Example:
+
+# Input: "1234"
+# Output: 1234
+
+
+def str_to_int(S):
+    num = 0
+    for i  in S:
+       num = num *10 + ord(i)-ord("0")
+    return num
+
+S = "1234"
+print(str_to_int(S))
+
+# Find the Longest Word in a Sentence
+# Problem: Given a sentence, find the longest word.
+# Example:
+
+# Input: "I love programming"
+# Output: "programming"
+
+def long_word_in_sentance(S:str):
+    sub_str = S.split(" ")
+    long_word = ""
+    for i in sub_str:
+        if long_word:
+            if len(long_word)<len(i):
+                long_word = i
+        else:
+            long_word = i
+    return long_word
+
+
+
+
+S="I love programming"
+print(long_word_in_sentance(S))
+
+# Capitalize the First Letter of Each Word
+# Problem: Given a sentence, capitalize the first letter of each word.
+# Example:
+
+# Input: "hello world"
+# Output: "Hello World"
+
+def capitalize_sen(S:str):
+    capitalize_sen1 = ""
+    change = 1
+    for i in S:
+        if change ==1:
+            a=ord(i)-32
+            capitalize_sen1 += chr(a)
+            change = 0
+        elif i == " ":
+            capitalize_sen1 += i
+            change = 1
+        else:
+            capitalize_sen1 += i
+    return capitalize_sen1
+        
+
+S = "hello world"
+print(capitalize_sen(S))
